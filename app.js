07dab82281746f29eb860ec37e12cffab657a8a2
Jane Smith <example@email.com>
2017-02-13 23:39:41 -0800

    cleaned up linter issues in JS

--- app.js
+++ app.js
@@ -38,10 +38,9 @@ var pike = {
       total = total + this.cookiesArray[i];
     }
 
-      var ul = document.getElementById('projectionsPike');
-      var pikeLi = document.createElement('li');
-      pikeLi.textContent = 'Total: ' + parseInt(total) + ' cookies'; //surround something with parseInt it rounds the number up.
-      ul.appendChild(pikeLi);
+
+    pikeLi.textContent = 'Total: ' + parseInt(total) + ' cookies'; //surround something with parseInt it rounds the number up.
+    ul.appendChild(pikeLi);
     console.log (parseInt(total));
   }
 };
@@ -49,10 +48,10 @@ pike.display();
 
 
 var Seatac = {
-location: 'Seatac Airport',
+  location: 'Seatac Airport',
   min: 3,
   max: 24,
-cookiesPerCustomer: 1.2,
+  cookiesPerCustomer: 1.2,
   cookiesArray: [],
   customersPerHour: function () {
     return Math.floor(Math.random() * (this.max - this.min) + this.min);
@@ -77,16 +76,15 @@ cookiesPerCustomer: 1.2,
       total = total + this.cookiesArray[i];
     }
 
-      var ul = document.getElementById('projectionsSeaTac');
-      var pikeLi = document.createElement('li');
-      pikeLi.textContent = 'Total: ' + parseInt(total) + ' cookies'; //surround something with parseInt it rounds the number up.
-      ul.appendChild(pikeLi);
+
+    pikeLi.textContent = 'Total: ' + parseInt(total) + ' cookies'; //surround something with parseInt it rounds the number up.
+    ul.appendChild(pikeLi);
     console.log (parseInt(total));
   }
 };
 Seatac.display();
 
-var seattleCenter= {
+var seattleCenter = {
   location: 'Seattle Center',
   min: 11,
   max: 38,
@@ -99,7 +97,6 @@ var seattleCenter= {
     return this.cookiesPerCustomer * this.customersPerHour();
   },
   storeHours:['6am: ','7am: ','8am: ','9am: ','10am: ','11am: ','12pm: ', '1pm: ', '2pm: ','3pm: ','4pm: ','5pm: ','6pm: ', '7pm: ','8pm: '],
-  
 
   display: function(){
     for(var i = 0; i < this.storeHours.length; i++){
@@ -116,17 +113,16 @@ var seattleCenter= {
       total = total + this.cookiesArray[i];
     }
 
-      var ul = document.getElementById('projectionsSeattleCenter');
-      var pikeLi = document.createElement('li');
-      pikeLi.textContent = 'Total: ' + parseInt(total) + ' cookies'; //surround something with parseInt it rounds the number up.
-      ul.appendChild(pikeLi);
+
+    pikeLi.textContent = 'Total: ' + parseInt(total) + ' cookies'; //surround something with parseInt it rounds the number up.
+    ul.appendChild(pikeLi);
     console.log (parseInt(total));
   }
 };
 seattleCenter.display();
 
 
-var capitolHill= {
+var capitolHill = {
   location: 'Capitol Hill',
   min: 20,
   max: 38,
@@ -139,7 +135,6 @@ var capitolHill= {
     return this.cookiesPerCustomer * this.customersPerHour();
   },
   storeHours:['6am: ','7am: ','8am: ','9am: ','10am: ','11am: ','12pm: ', '1pm: ', '2pm: ','3pm: ','4pm: ','5pm: ','6pm: ', '7pm: ','8pm: '],
-  
 
   display: function(){
     for(var i = 0; i < this.storeHours.length; i++){
@@ -156,10 +151,9 @@ var capitolHill= {
       total = total + this.cookiesArray[i];
     }
 
-      var ul = document.getElementById('projectionsCapitolHill');
-      var pikeLi = document.createElement('li');
-      pikeLi.textContent = 'Total: ' + parseInt(total) + ' cookies'; //surround something with parseInt it rounds the number up.
-      ul.appendChild(pikeLi);
+
+    pikeLi.textContent = 'Total: ' + parseInt(total) + ' cookies'; //surround something with parseInt it rounds the number up.
+    ul.appendChild(pikeLi);
     console.log (parseInt(total));
   }
 };
@@ -179,7 +173,6 @@ var alki = {
     return this.cookiesPerCustomer * this.customersPerHour();
   },
   storeHours:['6am: ','7am: ','8am: ','9am: ','10am: ','11am: ','12pm: ', '1pm: ', '2pm: ','3pm: ','4pm: ','5pm: ','6pm: ', '7pm: ','8pm: '],
-  
 
   display: function(){
     for(var i = 0; i < this.storeHours.length; i++){
@@ -196,10 +189,8 @@ var alki = {
       total = total + this.cookiesArray[i];
     }
 
-      var ul = document.getElementById('projectionsAlki');
-      var pikeLi = document.createElement('li');
-      pikeLi.textContent = 'Total: ' + parseInt(total) + ' cookies'; //surround something with parseInt it rounds the number up.
-      ul.appendChild(pikeLi);
+    pikeLi.textContent = 'Total: ' + parseInt(total) + ' cookies'; //parSInt () rounds the decimal up in JS
+    ul.appendChild(pikeLi);
     console.log (parseInt(total));
   }
 };
