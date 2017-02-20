a865412d82ab6e6ddb7f0b4eb366094905ae0780
Jane Smith <example@email.com>
2017-02-19 18:58:04 -0800

    code clean up

--- app.js
+++ app.js
@@ -1,7 +1,5 @@
 'use strict';
 
-
-
 var Store = function(storeName, storeLocation, minCookiesPerCustomer, maxCookiesPerCustomer, avgCookiesPurchased){
   this.name = storeName;
   this.location = storeLocation;
@@ -23,7 +21,6 @@ Store.prototype.cookiesPerHour = function(){
   return Math.floor(this.avgCookiesPurchased * this.customersPerHour());
 };
 
-
 Store.prototype.dailyProjection = function(){
   this.total = 0;
   var hourlyCookies;
@@ -31,11 +28,8 @@ Store.prototype.dailyProjection = function(){
     hourlyCookies = this.cookiesPerHour();
     this.cookiesArray.push(hourlyCookies);
     this.total = this.total + hourlyCookies;
-    console.log('total', this.total);
-    console.log(hourlyCookies);
   }
 };
-
 
 //fetches table element in html, creates text for new node, and appends new node onto table element for store name.
 Store.prototype.renderTableData = function(){
@@ -57,53 +51,17 @@ Store.prototype.renderTableData = function(){
   tableDataRow.appendChild(storeTotalsColumn);
 };
 
-//using keyword NEW to construct new objects.
-var pike = new Store ('Pike', '1st and Pike', 23, 65, 6.3);
-console.log(pike);
-var seatac = new Store ('SeaTac', 'SeaTac Airport', 3, 24, 1.2);
-console.log(seatac);
-var seattleCenter = new Store ('Seattle Center', 'Seattle Center', 11, 38, 3.7);
-console.log(seattleCenter);
-var capitolHill = new Store ('Capitol Hill', 'Capitol Hill', 20, 38, 2.3);
-console.log (capitolHill);
-var alki = new Store ('Alki', 'Alki', 2, 16, 4.6);
-console.log (alki);
-var storeLocations = [pike, seatac, seattleCenter, capitolHill, alki];
-
-var userForm  = document.getElementById('new_store');
-userForm.addEventListener('submit',submitHandler);
-this.newStoreFormArray = [];
-
 function submitHandler(event){
   event.preventDefault();
   var storeName = event.target.new_storeName.value;
   var minCookiesPerCustomer = parseInt(event.target.new_minCookiesPerCustomer.value);
   var maxCookiesPerCustomer = parseInt(event.target.max_CookiesPerCustomer.value);
   var avgCookiesPurchased = parseInt(event.target.new_avgCookiesPerCustomer.value);
-  console.log(storeName);
-  console.log(minCookiesPerCustomer);
-  console.log(maxCookiesPerCustomer);
-  console.log(avgCookiesPurchased);
 
   var newStoreInstance = new Store(storeName, storeName, minCookiesPerCustomer, maxCookiesPerCustomer, avgCookiesPurchased) ;
   newStoreInstance.dailyProjection(); //you must call the function in the for loop so it runs with every new instance
   newStoreInstance.renderTableData();
-  console.log(newStoreInstance);
 };
-
-//Calls the constructor function
-pike.dailyProjection();
-seatac.dailyProjection();
-seattleCenter.dailyProjection();
-capitolHill.dailyProjection();
-alki.dailyProjection();
-
-
-
-
-//creats new row in table to insert cookies per hour header
-var hours = ['','6am ','7am ','8am ','9am ','10am ','11am ','12pm ', '1pm ', '2pm ','3pm ','4pm ','5pm ','6pm ', '7pm ','8pm ', 'Daily Totals'];
-var tableElement = document.getElementById('dailyProjections');
 
 var renderHeader = function(){
   var headerRow = document.createElement('tr');
@@ -114,12 +72,6 @@ var renderHeader = function(){
     headerRow.appendChild(headerTh);
   }
 };
-renderHeader();
-pike.renderTableData();
-seatac.renderTableData();
-seattleCenter.renderTableData();
-capitolHill.renderTableData();
-alki.renderTableData();
 
 var renderFooter = function(){
   var hourlyProjectionsArray = hourlyProjections();
@@ -159,5 +111,40 @@ var hourlyProjections = function(){
   return hourlyProjectionsArray;
 };
 
-renderFooter();
 
+
+//using keyword NEW to construct new objects.
+var pike = new Store ('Pike', '1st and Pike', 23, 65, 6.3);
+var seatac = new Store ('SeaTac', 'SeaTac Airport', 3, 24, 1.2);
+var seattleCenter = new Store ('Seattle Center', 'Seattle Center', 11, 38, 3.7);
+var capitolHill = new Store ('Capitol Hill', 'Capitol Hill', 20, 38, 2.3);
+var alki = new Store ('Alki', 'Alki', 2, 16, 4.6);
+var storeLocations = [pike, seatac, seattleCenter, capitolHill, alki];
+
+var userForm  = document.getElementById('new_store');
+userForm.addEventListener('submit',submitHandler);
+this.newStoreFormArray = [];
+
+//Calls the constructor function
+pike.dailyProjection();
+seatac.dailyProjection();
+seattleCenter.dailyProjection();
+capitolHill.dailyProjection();
+alki.dailyProjection();
+
+
+
+
+//creats new row in table to insert cookies per hour header
+var hours = ['','6am ','7am ','8am ','9am ','10am ','11am ','12pm ', '1pm ', '2pm ','3pm ','4pm ','5pm ','6pm ', '7pm ','8pm ', 'Daily Totals'];
+var tableElement = document.getElementById('dailyProjections');
+
+renderHeader();
+
+pike.renderTableData();
+seatac.renderTableData();
+seattleCenter.renderTableData();
+capitolHill.renderTableData();
+alki.renderTableData();
+
+renderFooter();
